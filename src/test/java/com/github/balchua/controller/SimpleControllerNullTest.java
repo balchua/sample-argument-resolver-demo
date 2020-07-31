a2b90be89d78b70fd05c6167e41df1eba7cef3a0
balchua <balchua@yahoo.com>
2020-07-31 15:30:53 +0800

    test null rest template.

--- src/test/java/com/github/balchua/controller/SimpleControllerNullTest.java
+++ src/test/java/com/github/balchua/controller/SimpleControllerNullTest.java
@@ -1,22 +1,19 @@
 package com.github.balchua.controller;
 
+import com.github.balchua.LocalTestConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class SimpleControllerNullTest {
